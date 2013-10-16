53298b3b8234e5d4d512ef5939289529f0c9599f
imjenbas <mjbas@ittc.up.edu.ph>
2013-10-16 12:57:01 +0800

    Exercise 1 - Find the sum of numbers from x to y
    1. Ask for two integers (x and y)
    2. Find the sum of numbers from x to y

---
+++ add_post.php
@@ -0,0 +1,11 @@
+<html>
+	<head></head>
+	<title>Form Handling - Add_Post Sample PHP</title>
+	<body>
+	Hello you entered <br>
+	X: <?php echo$_POST['x']; ?><br>
+	Y: <?php echo$_POST['y']; ?><br>
+	The sum of numbers from X to Y =
+	
+	</body>
+</html>
---
+++ form-addition.php
@@ -0,0 +1,13 @@
+<html>
+	<head></head>
+	<title>Form Handling</title>
+	<body>
+	
+	<form action="add_post.php" method="post">
+	X: <input type="number" name="x"><br>
+	Y: <input type="number" name="y"><br>
+	<input type="submit">
+	</form>
+	
+	</body>
+</html>
