ed1fcb6239eb88c4705c57e79d0105181529befc
imjenbas <mjbas@ittc.up.edu.ph>
2013-10-16 12:56:12 +0800

    Form handling samples

---
+++ form-handling2.php
@@ -0,0 +1,13 @@
+<html>
+	<head></head>
+	<title>Form Handling</title>
+	<body>
+	
+	<form action="hello_post.php" method="post">
+	Name: <input type="text" name="name"><br>
+	Age: <input type="text" name="age"><br>
+	<input type="submit">
+	</form>
+	
+	</body>
+</html>
--- hello_post.php
+++ hello_post.php
@@ -1,10 +1,10 @@
 <html>
 	<head></head>
-	<title>Form Handling - Hello_Get PHP</title>
+	<title>Form Handling - Hello_Post PHP</title>
 	<body>
-	Hello <?php echo$_GET['name'];?>!<br>
+	Hello <?php echo$_POST['name']; ?>!<br>
 	<?php
-		if($_GET['age']>25)
+		if($_POST['age']>25)
 			echo 'You are young.';
 		else
 			echo 'You are young at heart.';
